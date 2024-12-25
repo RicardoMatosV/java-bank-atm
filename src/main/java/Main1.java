8455474a553e8469d17aaf357ffccfb2f1752310
Ricardo Matos <r.matos.learn@gmail.com>
2024-12-25 12:06:30 -0500

    Se agregó funcionalidad

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -1,18 +1,6 @@
 
-/*public class Main1 {
-    public static void main(String[] args) {
-        System.out.println("Hello, JavaBank ATM!");
-    }
-}*/
-
 public class Main1 {
     public static void main(String[] args) {
-        int result;
-        try {
-            result = 10 / 2; // Genera una excepción
-            System.out.println("El resultado es: " + result);
-        } catch (ArithmeticException e) {
-            System.out.println("No se puede dividir por cero!");
-        }
+        System.out.println("Welcome to JavaBank ATM! Version control with Git.");
     }
 }
